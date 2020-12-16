b5ff210e5eac248f3723739deab10bbb1fae5faa
jchans <jiangchenghans@gmail.com>
2020-12-16 20:12:18 +0800

    break index into parts

---
+++ parts/scripts.php
@@ -0,0 +1,2 @@
+<script src="<?= WEB_ROOT ?>js/jquery-3.5.1.js"></script>
+<script src="<?= WEB_ROOT ?>bootstrap-4.5.3-dist/js/bootstrap.bundle.js"></script>
--- public/index_.php
+++ public/index_.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    <p>好啦，雖然有點老套，但是，世界，你好。</p>
-</body>
-</html>
+<?php
+define('WEB_ROOT', '/noobphp/');
+
+// 待確認： PHP 的 include 的路徑，不是像網頁的路徑， WEB_ROOT 的寫法沒用
+// 用相對路徑寫會不會有安全疑慮？ 
+?>
+
+<?php include '../parts/html-head.php'; ?>
+<?php include '../parts/navbar.php'; ?>
+
+<?php include '../parts/include-test.php'; ?>
+
+<?php include '../parts/scripts.php'; ?>
+<?php include '../parts/html-foot.php'; ?>
