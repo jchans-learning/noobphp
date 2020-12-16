7357619d74cb125e402b2e2a1f1b95d5080f05ab
jchans <jiangchenghans@gmail.com>
2020-12-16 20:18:59 +0800

    change file directories

--- public/index_.php
+++ public/index_.php
@@ -1,14 +1,12 @@
 <?php
-define('WEB_ROOT', '/noobphp/');
+define('WEB_ROOT', '/noobphp/public/');
 
 // 待確認： PHP 的 include 的路徑，不是像網頁的路徑， WEB_ROOT 的寫法沒用
 // 用相對路徑寫會不會有安全疑慮？ 
 ?>
 
-<?php include '../parts/html-head.php'; ?>
-<?php include '../parts/navbar.php'; ?>
-
-<?php include '../parts/include-test.php'; ?>
-
-<?php include '../parts/scripts.php'; ?>
-<?php include '../parts/html-foot.php'; ?>
+<?php include __DIR__. '/parts/html-head.php'; ?>
+<?php include __DIR__. '/parts/navbar.php'; ?>
+<?php include __DIR__. '/parts/include-test.php'; ?>
+<?php include __DIR__. '/parts/scripts.php'; ?>
+<?php include __DIR__. '/parts/html-foot.php'; ?>
