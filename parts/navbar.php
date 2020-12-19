af51b7607f018410fcdd022fc8f2e1ba40b09cd1
LaohanJ <jiangchenghans@gmail.com>
2020-12-20 01:13:31 +0800

    add navbar items

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container">
 	  <a class="navbar-brand" href="#">課後複習</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
@@ -6,12 +7,16 @@
 
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
-	      <li class="nav-item active">
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">通訊錄列表</a>
 	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="#">Link</a>
+	        <a class="nav-link" href="#">新增通訊錄</a>
 	      </li>
 	    </ul>
+	    <ul class="navbar-nav">
+	    	<li class="nav-item">登入</li>
+	    </ul>
 	  </div>
-	</nav>
+	</div>
+</nav>
