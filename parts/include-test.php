1be4d226eb920046b15dbb2dd2813a0d5026fbb8
LaohanJ <jiangchenghans@gmail.com>
2020-12-19 21:16:09 +0800

    Add index text

--- parts/include-test.php
+++ parts/include-test.php
@@ -11,11 +11,11 @@
 			<p>PHP and MySQL 上課的內容主要是做出一個通訊錄， admin 使用者登入之後可以新增，讀取，修改，刪除通訊錄的資料。</p>
 
 			<ul>
-				<li>navbar</li>
+				<li>架構</li>
 					<ul>
-						<li>通訊錄列表(R)</li>
+						<li>通訊錄列表</li>
 							<ul>
-								<li>讀取</li>
+								<li>讀取通訊錄資料(R)</li>
 									<ul>
 										<li>通訊錄列表</li>
 										<li>單頁顯示筆數設定</li>
@@ -29,12 +29,12 @@
 											<li>最後一頁按鈕</li>
 										</ul>
 									</ul>
-								<li>修改資料(U ， admin 登入後)</li>
-								<li>刪除資料(D ， admin 登入後)</li>
+								<li>修改單筆資料(U ， admin 登入後)</li>
+								<li>刪除單筆資料(D ， admin 登入後)</li>
 							</ul>
-						<li>新增通訊錄(C ， admin 登入後)</li>
+						<li>新增通訊錄( admin 登入後可連結，未登入連到登入頁面)</li>
 							<ul>
-								<li>新增</li>
+								<li>新增單筆資料(C ， admin 登入後)</li>
 							</ul>
 							
 						<li>登入</li>
