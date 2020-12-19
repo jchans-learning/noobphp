799c42c7fd32d4380fd5741c8cdd0d04f9270a95
LaohanJ <jiangchenghans@gmail.com>
2020-12-19 14:43:46 +0800

    change to local dev

--- index_.php
+++ index_.php
@@ -1,9 +1,9 @@
 <?php
-// 本地端測試的時候用
-// define('WEB_ROOT', '/noobphp/');
+// 開發中，本地端測試的時候用
+define('WEB_ROOT', '/noobphp/');
 
 // push 到 Heroku 時改為
-define('WEB_ROOT', '/');
+// define('WEB_ROOT', '/');
 
 // 待確認： PHP 的 include 的路徑，不是像網頁的路徑， WEB_ROOT 的寫法沒用
 // 用相對路徑寫會不會有安全疑慮？ 
