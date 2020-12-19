e2cb189ba25a10b99cbee720a7ecbbe35b14392b
LaohanJ <jiangchenghans@gmail.com>
2020-12-20 01:27:34 +0800

    Add todo items

--- parts/include-test.php
+++ parts/include-test.php
@@ -1,15 +1,22 @@
 <div class="container">
+
+	<div class="r" p-3 mb-2>
+		<h1>工作記事</h1>
+	</div>
+
 	<div class="row p-3 mb-2">
 		<div class="">
+			<h2>開張</h2>
 			<p>好啦，雖然有點老套，但是，世界，你好。</p>
 			<p>從頭開始做</p>
 			<p><a href="/">確認更新狀況</a></p>
 		</div>
 	</div>
+
 	<div class="row p-3 mb-2">
 		<div class="">
+			<h2>複習規劃</h2>
 			<p>PHP and MySQL 上課的內容主要是做出一個通訊錄， admin 使用者登入之後可以新增，讀取，修改，刪除通訊錄的資料。</p>
-
 			<ul>
 				<li>架構</li>
 					<ul>
@@ -41,10 +48,31 @@
 						<li>登入</li>
 							<ul>
 								<li>admin 使用者</li>
+								<li>編輯 admin 使用者資料</li>
+									<ul>
+										<li>上傳頭像照片</li>
+										<li>帳號名稱不可更改</li>
+										<li>暱稱（可留空）</li>
+										<li>確認舊密碼</li>
+										<li>更改密碼（可留空）</li>
+									</ul>
 							</ul>
 					</ul>
 			</ul>
-
 		</div>
-	</div>	
+	</div>
+
+	<div class="row p-3 mb-2">
+		<div>
+			<h2>工作筆記</h2>
+			<ul>
+				<li>
+					<a href="https://github.com/jchans/noobphp/commit/af51b7607f018410fcdd022fc8f2e1ba40b09cd1#diff-b07e48f0f2dc6c53e55b040c07454d668afb2670ebcc26da0a7e44397b630a93">
+						製作 navbar ，加上 div container 對齊。
+					</a>
+				</li>
+			</ul>			
+		</div>
+	</div>
+
 </div>
