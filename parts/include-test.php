9d3b018d7f0b5093b12bbc9ce6473dc719bee76c
LaohanJ <jiangchenghans@gmail.com>
2020-12-19 20:57:20 +0800

    Add index text

--- parts/include-test.php
+++ parts/include-test.php
@@ -1,36 +1,48 @@
 <div class="container">
-	<div class="row">
-		<div>
+	<div class="row p-3 mb-2">
+		<div class="">
 			<p>好啦，雖然有點老套，但是，世界，你好。</p>
 			<p>從頭開始做</p>
 			<p><a href="/">確認更新狀況</a></p>
+		</div>
+	</div>
+	<div class="row p-3 mb-2">
+		<div class="">
 			<p>PHP and MySQL 上課的內容主要是做出一個通訊錄， admin 使用者登入之後可以新增，讀取，修改，刪除通訊錄的資料。</p>
 
 			<ul>
-				<li>分頁 navbar </li>
+				<li>navbar</li>
 					<ul>
 						<li>通訊錄列表(R)</li>
-						<li>新增通訊錄(C)</li>
+							<ul>
+								<li>讀取</li>
+									<ul>
+										<li>通訊錄列表</li>
+										<li>單頁顯示筆數設定</li>
+										<li>頁數條</li>
+										<ul>
+											<li>顯示頁數</li>
+											<li>顯示指定數量的頁數</li>
+											<li>上一頁按鈕</li>
+											<li>下一頁按鈕</li>
+											<li>第一頁按鈕</li>
+											<li>最後一頁按鈕</li>
+										</ul>
+									</ul>
+								<li>修改資料(U ， admin 登入後)</li>
+								<li>刪除資料(D ， admin 登入後)</li>
+							</ul>
+						<li>新增通訊錄(C ， admin 登入後)</li>
+							<ul>
+								<li>新增</li>
+							</ul>
+							
 						<li>登入</li>
+							<ul>
+								<li>admin 使用者</li>
+							</ul>
+						<li>搜尋資料的功能</li>
 					</ul>
-				<li>讀取</li>
-					<ul>
-						<li>通訊錄列表</li>
-						<li>單頁顯示筆數設定</li>
-						<li>頁數條</li>
-						<ul>
-							<li>顯示頁數</li>
-							<li>顯示指定數量的頁數</li>
-							<li>上一頁按鈕</li>
-							<li>下一頁按鈕</li>
-							<li>第一頁按鈕</li>
-							<li>最後一頁按鈕</li>
-						</ul>
-					</ul>
-				<li>新增</li>
-				<li>修改</li>
-				<li>刪除</li>
-				<li>admin 使用者</li>
 			</ul>
 
 		</div>
