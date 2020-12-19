0932c2c45134d837a6ca224b59ec8342f069e1ef
LaohanJ <jiangchenghans@gmail.com>
2020-12-20 01:47:57 +0800

    move WEB_ROOT setting code and make page folder

--- pages/addressbook-list.php
+++ pages/addressbook-list.php
@@ -1,9 +1,7 @@
-<?php 
-// 開始開發前記得去 parts/html-head.php 確認一下 WEB_ROOT 的設定
- ?>
+<?php // address book list ?>
 
 <?php include __DIR__. '/parts/html-head.php'; ?>
 <?php include __DIR__. '/parts/navbar.php'; ?>
-<?php include __DIR__. '/parts/index-content.php'; ?>
+
 <?php include __DIR__. '/parts/scripts.php'; ?>
 <?php include __DIR__. '/parts/html-foot.php'; ?>
