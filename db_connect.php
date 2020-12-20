6d6256fa936f323867e02f3fe908e2ca7e924b63
LaohanJ <jiangchenghans@gmail.com>
2020-12-20 16:16:52 +0800

    correct typo

--- db_connect.php
+++ db_connect.php
@@ -2,10 +2,10 @@
 
 include 'db_key_noobphp.php';
 
-$dsn = "mysql:host={$db_host}; dbname={db_name};charset=utf8";
+$dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
 
 $pdo_options = [
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCETION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 ];
