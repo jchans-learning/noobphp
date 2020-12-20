28067ef89f8d44a0976a30878e315fcbdd3e23ce
LaohanJ <jiangchenghans@gmail.com>
2020-12-20 15:54:09 +0800

    Setup addressbook-list-api link with db

--- addressbook-list-api.php
+++ addressbook-list-api.php
@@ -1,1 +1,16 @@
 <?php 
+require __DIR__ . '/db_connect.php';
+
+$pageName = 'addressbook-list';
+
+$p_sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC ");
+
+$statement = $pdo->query($p_sql);
+
+$rows = $statement->fetchAll();
+
+$output = [
+	'rows' => $rows,
+];
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
