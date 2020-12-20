65d7e3f3379495c46e50e6c16ec16e40d6271bd7
LaohanJ <jiangchenghans@gmail.com>
2020-12-21 01:44:36 +0800

    Add delete feature

---
+++ addressbook-delete.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__. '/db_connect.php';
+
+if (isset($_GET['sid']){
+	$sid = intval($_GET['sid']);
+	$pdo->query(DELETE FROM `address_book` WHERE sid=$sid);
+}
+
+$backto = 'addressbook-list.php';
+if (isset($_SERVER['HTTP_REFERER'])) {
+	$backto = $_SERVER['HTTP_REFERER'];
+}
+
+header('Location: ' . $backto)
