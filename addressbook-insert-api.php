42e4a56566aa32bd29de4b82703241cddb996c21
LaohanJ <jiangchenghans@gmail.com>
2020-12-21 11:35:35 +0800

    fix typo

--- addressbook-insert-api.php
+++ addressbook-insert-api.php
@@ -1,8 +1,13 @@
 <?php 
-require __DIR__. 'db_connect.php';
+require __DIR__. '/db_connect.php';
 
 $output = [
 	'success' => false,
 	'code' => 0,
 	'error' => '參數不足',
-]
+];
+
+if (!isset($_POST['name']) and !isset($_POST['email'])) {
+	echo json_encode($output, JSON_UNESCAPED_UNICODE);
+}
+
