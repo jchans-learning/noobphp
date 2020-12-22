6ace49b3a02fcdaab0bf395958ef09eeb0cef440
LaohanJ <jiangchenghans@gmail.com>
2020-12-22 19:45:06 +0800

    add session start and pageName

--- index_.php
+++ index_.php
@@ -2,6 +2,15 @@
 // 開始開發前記得去 parts/html-head.php 確認一下 WEB_ROOT 的設定
  ?>
 
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$pageName = 'Index1';
+?>
+
+
 <?php include __DIR__. '/parts/html-head.php'; ?>
 <?php include __DIR__. '/parts/navbar.php'; ?>
 <?php include __DIR__. '/parts/index-content.php'; ?>
