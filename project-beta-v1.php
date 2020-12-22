4da704dfe8742e8531016471d946948fc13466ea
LaohanJ <jiangchenghans@gmail.com>
2020-12-22 09:33:41 +0800

    add container

--- project-beta-v1.php
+++ project-beta-v1.php
@@ -5,7 +5,9 @@
 <?php include __DIR__. '/parts/html-head.php'; ?>
 <?php include __DIR__. '/parts/navbar.php'; ?>
 
-<h1>小專試作 v1 </h1>
+<div class="container">
+    <h1>小專試作 v1 </h1>
+</div>
 
 <?php include __DIR__. '/parts/scripts.php'; ?>
 <?php include __DIR__. '/parts/html-foot.php'; ?>
