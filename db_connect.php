25b34ba1980acb9ff7ecadb8e1b365e44d29ad0e
jchans <jiangchenghans@gmail.com>
2020-12-23 02:09:07 +0800

    heroku test

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,28 @@
 <?php
 
-include 'db_key_noobphp.php';
+// 開發中，本地端測試的時候用
+// include 'db_key_noobphp.php';
+// $dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
 
-$dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
+// push 到 Heroku 時改為
+//
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+//
+// push 到 Heroku 時改為 End
+
+
 
 $pdo_options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
