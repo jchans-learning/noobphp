bfac4a140319083f9eb201f45586eb9409e1aef3
jchans <jiangchenghans@gmail.com>
2020-12-23 18:17:15 +0800

    try delete

--- addressbook-delete.php
+++ addressbook-delete.php
@@ -2,9 +2,18 @@
 require __DIR__. '/is_admin.php';
 require __DIR__. '/db_connect.php';
 
+// Local Develop with MySQL
+//
+// if (isset($_GET['sid'])) {
+// 	$sid = intval($_GET['sid']);
+// 	$pdo->query("DELETE FROM `address_book` WHERE sid=$sid");
+// }
+
+// Heroku with PostgreSQL
+//
 if (isset($_GET['sid'])) {
 	$sid = intval($_GET['sid']);
-	$pdo->query("DELETE FROM `address_book` WHERE sid=$sid");
+	$pdo->query("DELETE FROM address_book WHERE sid=$sid");
 }
 
 $backto = 'addressbook-list.php';
