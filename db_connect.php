91a7c39c516878be95e0fd07c513a4f0378f1d70
jchans <jiangchenghans@gmail.com>
2020-12-23 15:27:30 +0800

    change to heroku

--- db_connect.php
+++ db_connect.php
@@ -1,36 +1,29 @@
 <?php
 
-// push 到 Heroku 時改為
-//
-// $dbopts = parse_url(getenv('DATABASE_URL'));
-// $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-//                array(
-//                 'pdo.server' => array(
-//                    'driver'   => 'pgsql',
-//                    'user' => $dbopts["user"],
-//                    'password' => $dbopts["pass"],
-//                    'host' => $dbopts["host"],
-//                    'port' => $dbopts["port"],
-//                    'dbname' => ltrim($dbopts["path"],'/')
-//                    )
-//                )
-// );
-//
-// push 到 Heroku 時改為 End
-
 // 開發中，本地端測試的時候用
-include 'db_key_noobphp.php';
-$dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
+// include 'db_key_noobphp.php';
+// $dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
 
-$pdo_options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-];
+// $pdo_options = [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+// 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+// 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+// ];
 
+// $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
 
-$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+// if (!isset($_SESSION)) {
+// 	session_start();
+// }
 
-if (!isset($_SESSION)) {
-	session_start();
-}
+// push 到 Heroku 時改為
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
