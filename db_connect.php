c83dfa4f4872cab8cad79948c9e950cffc88c811
jchans <jiangchenghans@gmail.com>
2020-12-23 09:43:06 +0800

    organize codes

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,5 @@
 <?php
 
-// 開發中，本地端測試的時候用
-include 'db_key_noobphp.php';
-$dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
-
 // push 到 Heroku 時改為
 //
 // $dbopts = parse_url(getenv('DATABASE_URL'));
@@ -22,7 +18,9 @@ $dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
 //
 // push 到 Heroku 時改為 End
 
-
+// 開發中，本地端測試的時候用
+include 'db_key_noobphp.php';
+$dsn = "mysql:host={$db_host}; dbname={$db_name};charset=utf8";
 
 $pdo_options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
